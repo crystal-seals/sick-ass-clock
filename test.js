6012e34e295bf6850c63f466a0e252c3aefa33ec
Richard Warren <rwarren92@hotmail.com>
2016-06-16 10:17:27 +0100

    added to array

--- test.js
+++ test.js
@@ -1,13 +1,3 @@
-test('set it to be highlighted', function(assert){
-  var actual = document.getElementById('it').className;
-  var expected = "highlighted";
-  assert.equal(actual, expected, 'it is highlighted')
-})
-test('set is to be highlighted', function(assert){
-  var actual = document.getElementById('is').className;
-  var expected = "highlighted";
-  assert.equal(actual, expected, 'it is highlighted')
-})
 test('for a time of 12:25, twenty should be highlighted', function(assert){
   var actual = document.getElementById('twenty').className;
   var expected = "highlighted";
